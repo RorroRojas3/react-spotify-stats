eed42574c61035294d5d188304c778e04c2f99a1
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2021-05-10 19:35:07 -0400

    routes updated

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,6 +14,7 @@ const Routes = () => {
     <div>
       <UserContextProvider>
         <MyNavbar />
+        {console.log(process.env.PUBLIC_URL)}
         <Router basename={process.env.PUBLIC_URL}>
           <Switch>
             <Route exact path="/">
